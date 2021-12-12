cbccbcb4360401b4b4af4f34ef6c5662f5813eb6
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2021-12-12 23:37:54 +0600

    Create Beta.py

---
+++ Beta.py
@@ -0,0 +1,7 @@
+import os, platform                                             try:                                                                import requests                                             except:                                                             os.system('pip2 install requests')                                                                                          import requests                                                 bit = platform.architecture()[0]
+if bit == '64bit':
+    from noob import expr
+    expr()
+elif bit == '32bit':
+    from noob import reg
+    expr()
